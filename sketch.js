e6dfd44abe69b8034ca7544cf1df51b60b235d8a
julian-q <julianhquevedo@gmail.com>
2021-09-16 17:04:47 -0700

    added trajectory support

--- sketch.js
+++ sketch.js
@@ -1,19 +1,15 @@
-let infoImage;
-
-function preload() {
-  infoImage = loadImage("img/baseline_info_white_24dp.png");
-}
-
 var canvas;
 var fInput;
 var gInput;
 var eqRenderer;
+let link;
 
 let promptPosition;
-let eqPosition
+let eqPosition;
+let eqSize;
 let stepSize;
-let vLength;
-let scale;
+let vScale;
+let pixelScale;
 let field;
 let origin;
 let validEq;
@@ -24,6 +20,7 @@ function setup() {
 
   promptPosition = createVector(50, 50);
   eqPosition = createVector(50, 200);
+  eqSize = createVector(0, 0);
 
   fInput = createInput("-y");
   fInput.class("asciimath");
@@ -37,20 +34,34 @@ function setup() {
   eqRenderer.position(100, 100);
   eqRenderer.position(eqPosition.x, eqPosition.y);
 
+  link = createA("http://asciimath.org/", "<img src='img/info_white_24dp.svg' width=15px>");
+  link.position(promptPosition.x + 90, promptPosition.y + 2);
+
+  origin = createVector(width/2, height/2);
   stepSize = createVector(50, 50);
   vLength = 15;
-  scale = 2;
+  pixelScale = 2;
   field = createVector(0, 0);
   updateField(fInput.value(), gInput.value());
-  origin = createVector(width/2, height/2);
   validEq = true;
 }
 
+let fps = 0;
+
 function draw() {
-  background(0);;
+  background(0);
   drawPlot();
   drawPrompt();
   drawLaTeX();
+  drawTrajectories();
+  // if (frameCount % 30 == 0) {
+  //   fps = frameRate();
+  // }
+  // noStroke();
+  // fill(255);
+  // textFont("Courier New");
+  // textSize(24);
+  // text("" + fps, width - 70, 20);
 }
 
 function drawPlot() {
@@ -67,25 +78,40 @@ function drawPlot() {
 
 function drawAxes() {
   stroke(255);
-  strokeWeight(4);
+  strokeWeight(1);
 	line(origin.x, 0, origin.x, height);
 	line(0, origin.y, width, origin.y);
 }
 
 function drawField(field) {
-	push();
-	translate(origin.x, origin.y);
-
-  let start = createVector(
-    -origin.x + (origin.x % stepSize.x),
-    -origin.y + (origin.y % stepSize.y)
+  let pixelStart = createVector(
+    (origin.x % stepSize.x) - stepSize.x,
+    (origin.y % stepSize.y) - stepSize.y
   );
-
-  for (let i = start.x; i < width; i += stepSize.x) {
-    for (let j = start.y; j < height; j += stepSize.y) {
+  let pixelStop = createVector(
+    width + stepSize.x,
+    height + stepSize.y
+  );
+  let graphStart = pixelToGraph(pixelStart);
+  let graphStop = pixelToGraph(pixelStop);
+  colorMode(HSB);
+  for (let i = start.x; i <= stop.x; i += stepSize.x) {
+    strokeWeight(1);
+    stroke(210, 360, 180, 50);
+    for (let c = i; c < i + stepSize.x; c += stepSize.x / 4) {
+      line(c, 0, c, height);
+      stroke(210, 360, 30, 50);
+    }
+    for (let j = start.y; j <= stop.y; j += stepSize.y) {
+      strokeWeight(1);
+      stroke(210, 360, 180, 50);
+      for (let r = j; r < j + stepSize.y; r += stepSize.y / 4) {
+        line(0, r, width, r);
+        stroke(210, 360, 30, 50);
+      }
 			let scope = {
-        x: i * scale,
-        y: j * scale
+        x: (i - origin.x) * pixelScale,
+        y: (-j + origin.y) * pixelScale
       };
       let v = createVector(
         field.x.evaluate(scope),
@@ -94,27 +120,26 @@ function drawField(field) {
       // Map the magnitude to a color of corresponding "warmth."
       let colorDeg = map(v.mag(), 0, 1000, 180, 0, true);
       // Normalize for drawing purposes.
-      v.normalize();
-      v.mult(vLength);
+      // v.normalize();
+      v.mult(vScale);
       // Compute the verticies of the triangle that will make up the arrowhead.
       let trianglePoints = [
         createVector(
           i + v.x,
-          j + v.y
-        ),
-        createVector(
-          i + 0.75*v.x - 0.2*v.y,
-          j + 0.75*v.y + 0.2*v.x
+          j - v.y
         ),
         createVector(
           i + 0.75*v.x + 0.2*v.y,
-          j + 0.75*v.y - 0.2*v.x
+          j - 0.75*v.y + 0.2*v.x
+        ),
+        createVector(
+          i + 0.75*v.x - 0.2*v.y,
+          j - 0.75*v.y - 0.2*v.x
         )
       ];
       // Draw line and arrowhead using calculated color and points.
-      colorMode(HSB, 360);
       stroke(colorDeg, 360, 360);
-      strokeWeight(2);
+      strokeWeight(0.1 * v.mag());
       fill(colorDeg, 360, 360);
       line(
         i, j,
@@ -127,15 +152,14 @@ function drawField(field) {
       );
     }
   }
-
-	pop();
 }
 
 function drawPrompt() {
-  stroke(255, 100);
+  stroke(255, 50);
   strokeWeight(1)
   fill(0, 180);
   rect(promptPosition.x, promptPosition.y, 365, 75);
+  rect(promptPosition.x, promptPosition.y + 250, 180, 20);
 
   noStroke();
   fill(255);
@@ -143,7 +167,7 @@ function drawPrompt() {
   textSize(12);
   textStyle(NORMAL);
   text("AsciiMath Input", promptPosition.x + 5, promptPosition.y + 14);
-  image(infoImage, promptPosition.x + 90, promptPosition.y + 2);
+  text("Double-click to add a trajectory", promptPosition.x + 5, promptPosition.y + 264);
   textFont("Courier New");
   textSize(16);
   textStyle(BOLD);
@@ -162,37 +186,10 @@ function drawPrompt() {
 }
 
 function drawLaTeX() {
-  // var elhg;
-  // var elwg;
-
-  // var checkEx = setInterval(function () {
-  //   let wrap = eqRenderer.elt;
-  //   var text = wrap.getElementsByClassName('MathJax')[0];
-  //   if (text) {
-  //     elHeight = wrap.getBoundingClientRect().height;
-  //     elWidth = wrap.getBoundingClientRect().width;
-  //
-  //     if (elhg === elHeight && elwg === elWidth) {
-  //         console.log(elHeight, elWidth);
-  //         clearInterval(checkEx);
-  //     }
-  //
-  //     elhg = elHeight;
-  //     elwg = elWidth;
-  //
-  //     stroke(255, 100);
-  //     strokeWeight(1)
-  //     fill(0, 180);
-  //     rect(eqPosition.x, eqPosition.y, elwg, elhg);
-  //   }
-  // }, 100);
-
-  noStroke();
-  fill(255);
-  textFont("Helvetica");
-  textSize(12);
-  textStyle(NORMAL);
-
+  stroke(255, 50);
+  strokeWeight(1)
+  fill(0, 180);
+  rect(eqPosition.x, eqPosition.y, eqSize.x - 3, eqSize.y + 6);
   eqRenderer.position(eqPosition.x - 2, eqPosition.y + 3);
 }
 
@@ -200,7 +197,6 @@ function drawBracket(x, y, w, h, opening) {
   if (!opening) {
     w *= -1;
   }
-
   line(x, y, x, y + h);
   line(x, y, x + w, y);
   line(x, y + h, x + w, y + h);
@@ -211,20 +207,28 @@ function updateField(e1, e2) {
 	field.y = math.parse(e2);
   field.x.compile();
   field.y.compile();
-
 	validEq = true;
-
 	try {
 		let scope = {
 			x: 0,
 			y: 0
 		};
-		field.x.evaluate(scope);
-		field.y.evaluate(scope);
+		let originMagnitude = createVector(
+      field.x.evaluate(scope),
+		  field.y.evaluate(scope)
+    ).mag();
+    scope = {
+      x: (width - origin.x) * pixelScale,
+      y: (-height + origin.y) * pixelScale
+    };
+    let distantMagnitude = createVector(
+      field.x.evaluate(scope),
+		  field.y.evaluate(scope)
+    ).mag();
+    vScale = 50.0 / Math.max(originMagnitude, distantMagnitude);
 	} catch(error) {
 		validEq = false;
 	}
-
 	if (validEq) {
 		eqRenderer.html(
 			"`[[f(x,y)], [g(x,y)]] = [[" + e1 + "], [" + e2 + "]]`"
@@ -235,6 +239,29 @@ function updateField(e1, e2) {
 		);
 	}
 	MathJax.typesetPromise();
+
+  let elhg;
+  let elwg;
+  let checkEx = setInterval(function () {
+    var wrap = eqRenderer.elt;
+    var text = wrap.getElementsByClassName('MathJax')[0];
+    if (text) {
+      elHeight = wrap.getBoundingClientRect().height;
+      elWidth = wrap.getBoundingClientRect().width;
+
+      if (elhg === elHeight && elwg === elWidth) {
+          clearInterval(checkEx);
+      }
+
+      elhg = elHeight;
+      elwg = elWidth;
+
+      eqSize.x = elWidth;
+      eqSize.y = elHeight;
+    }
+  }, 100);
+
+  trajectories = [];
 }
 
 function updateOrigin() {
@@ -249,11 +276,16 @@ function keyPressed() {
 }
 
 function mouseWheel(event) {
-  scale -= event.delta * 0.01;
+  pixelScale -= event.delta * 0.01;
 
-  if (scale > 0) {
-    scale = 0;
+  if (pixelScale < 0) {
+    pixelScale = 0;
   }
+
+  let sign = event.delta < 0 ? 1 : -1;
+
+  stepSize.x -= Math.pow(Math.abs(event.delta) * 0.01, 2) * sign;
+  stepSize.y -= Math.pow(Math.abs(event.delta) * 0.01, 2) * sign;
 
   return false;
 }
@@ -264,4 +296,66 @@ function windowResized() {
 
 function clamp(value, min, max) {
   return Math.min(Math.max(value, min), max);
+}
+
+let trajectories = [];
+
+function doubleClicked() {
+  let x = (mouseX - origin.x) * pixelScale;
+  let y = (-mouseY + origin.y) * pixelScale;
+
+  let trajectory = [createVector(x, y)];
+
+  for (let i = 0; i < 1000; i++) {
+    let scope = {
+      x: trajectory[trajectory.length - 1].x,
+      y: trajectory[trajectory.length - 1].y
+    }
+    let delta = createVector(
+      field.x.evaluate(scope),
+      field.y.evaluate(scope)
+    );
+    delta.mult(0.01);
+    trajectory.push(p5.Vector.add(trajectory[trajectory.length - 1], delta));
+  }
+
+  trajectories.push(trajectory);
+}
+
+
+// 0.5x * (1 - (x + 0.5y) / 200)
+// 0.5y * (1 - (y - 0.5x) / 200)
+
+function drawTrajectories() {
+  for (let trajectory of trajectories) {
+    if (trajectory.length > 1) {
+      stroke(255);
+      strokeWeight(2);
+      for (let i = 1; i < trajectory.length; i++) {
+        let start = createVector(
+          (trajectory[i - 1].x /  pixelScale) + origin.x,
+          (trajectory[i - 1].y / -pixelScale) + origin.y
+        );
+        let stop = createVector(
+          (trajectory[i].x /  pixelScale) + origin.x,
+          (trajectory[i].y / -pixelScale) + origin.y
+        );
+        line(start.x, start.y, stop.x, stop.y);
+      }
+    }
+  }
+}
+
+function graphToPixel(graphCoord) {
+  return createVector(
+    (graphCoord.x /  pixelScale) + origin.x,
+    (graphCoord.y / -pixelScale) + origin.y
+  );
+}
+
+function pixelToGraph(pixelCoord) {
+  return createVector(
+    (pixelCoord.x - origin.x) * pixelScale,
+    (-pixelCoord.y + origin.y) * pixelScale
+  );
 }
